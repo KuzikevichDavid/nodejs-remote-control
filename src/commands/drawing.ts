2329ed3db68281f122655c0ef0ef99218ec822fe
DavydKuzikevich <wliorjroo@gmail.com>
2023-01-20 16:28:18 +0300

    feat: add drawing circle

--- src/commands/drawing.ts
+++ src/commands/drawing.ts
@@ -20,4 +20,41 @@ export namespace drawCommands {
     }
     await mouse.drag(rectangle);
   };
+
+  export const drawCircle = async (radius: number) => {
+    const start = await mouse.getPosition();
+    const x = start.x,
+      y = start.y + radius;
+    const circleSectors = new Array<Array<Point>>(8);
+    for (let i = 0; i < circleSectors.length; i++) circleSectors[i] = new Array<Point>();
+    let X = 0,
+      Y = radius,
+      delta = 3 - 2 * radius,
+      error = 0;
+    while (X <= Y) {
+      circleSectors[3].push(new Point(x + X, y + Y));
+      circleSectors[4].push(new Point(x - X, y + Y));
+      circleSectors[0].push(new Point(x + X, y - Y));
+      circleSectors[7].push(new Point(x - X, y - Y));
+      circleSectors[2].push(new Point(x + Y, y + X));
+      circleSectors[5].push(new Point(x - Y, y + X));
+      circleSectors[1].push(new Point(x + Y, y - X));
+      circleSectors[6].push(new Point(x - Y, y - X));
+      error = 2 * (delta + Y) - 1;
+      if (delta < 0 && error <= 0) {
+        delta += 2 * ++X + 1;
+        continue;
+      }
+      if (delta > 0 && error > 0) {
+        delta -= 2 * --Y + 1;
+        continue;
+      }
+      delta += 2 * (++X - --Y);
+    }
+    circleSectors.forEach((val, i, array) => {
+      if (i % 2 === 1) array[i] = array[i].reverse();
+    });
+    const circle = circleSectors.flat();
+    await mouse.drag(circle);
+  };
 }
